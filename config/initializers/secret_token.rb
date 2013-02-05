3e8c56f8c48ef174b76316b5a356fcd3ccf79bb0
whigs <dantmeyer@gmail.com>
2013-02-04 19:36:41 -0600

    vcard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beepboop::Application.config.secret_token = 'f9da538d806433be1c9b95a4ff61efbcdfeee2244310fd153faa8e3be7717dd94ed01ffbfa89fd8375987ddd43d85bb0b509d1e3104f4972be49d42d1de8ed22'
